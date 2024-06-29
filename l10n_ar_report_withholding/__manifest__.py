6aa161d60871b44dcf5e7b5571c825918be4df36
franco0310 <francocorvalan2@gmail.com>
2024-06-29 12:30:26 -0300

    ajustes ultimos de a2systems + cambios aplicados por FC

--- l10n_ar_report_withholding/__manifest__.py
+++ l10n_ar_report_withholding/__manifest__.py
@@ -3,14 +3,16 @@
     'author': "A2 Systems,Moldeo Interactive,ADHOC SA,Odoo Community Association (OCA)",
     'category': 'Localization/Argentina',
     'depends': [
-        'l10n_ar_account_withholding','account_payment_group',
+        'account',
+        'l10n_ar_account_withholding',
+        'account_payment_group',
     ],
     'installable': True,
     'license': 'AGPL-3',
     'name': 'l10n_ar_report_withholding',
     'data': [
         'report_payment_withholding.xml',
-        # 'withholding_view.xml',
+        'withholding_view.xml',
     ],
     'demo': [
 
